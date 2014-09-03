a2f713a12c2eda2da349f5f316f614f274fed202
Lu Nelson <lunelson@gmail.com>
2014-09-03 09:52:34 +0200

    getting it together

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-sass-ln
-=======
+## Sass-LN
 
-A framework for modular query-based layout and typography
+Sass-LN is a framework for modularly-scaled responsive layout and optically-aligned typography. It makes extensive use of maps (or 'list-maps') in Sass.
+
+This frame work is still under development and not documented, but feel free to look around.
+
+---
+
+![](ln3.jpg)
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+//////////////////
+// definitions //
+//////////////////
+
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+
+//////////
+// sass //
+//////////
+
+// task: sass
+gulp.task('sass', function () {
+    gulp.src('test/test.scss')
+        .pipe(sass({
+            errLogToConsole: false,
+            onError: function(err) {
+                console.log(err);
+            }
+        }))
+        .pipe(gulp.dest('test'));
+});
+
+///////////
+// watch //
+///////////
+
+gulp.task('watch', function () {
+    gulp.watch('test/test.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
