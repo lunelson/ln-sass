0f23de5d8a04592ecaff7bc80e26257d8fd4b9a8
Lu Nelson <lunelson@gmail.com>
2014-09-16 11:35:07 +0200

    gulpfile update for browsersync
    copied over and modified following browsersync config guide

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 // definitions //
 //////////////////
 
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync');
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var filter       = require('gulp-filter');
 
 //////////////////////////////////////
 // sass + autoprefixer + sourcemaps //
@@ -16,15 +18,33 @@ gulp.task('sass', function () {
         .pipe(sourcemaps.init())
         .pipe(sass({
             errLogToConsole: false,
-            onError: function(err) { console.log(err); }
+            onError: function(err) {
+                browserSync.notify(err, 2000);
+                console.log(err); }
         }))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ['last 3 versions'],
             cascade: false
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('test'));
+        .pipe(gulp.dest('test'))
+        .pipe(filter('*.css')) // necessary?
+        .pipe(browserSync.reload({stream:true}));
 });
+
+/////////////////
+// browsersync //
+/////////////////
+
+// task: browser-sync startup; runs sass--app and jekyll-build then starts server
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync({
+        server: {
+            baseDir: 'test'
+        }
+    });
+});
+
 
 ///////////
 // watch //
@@ -32,6 +52,8 @@ gulp.task('sass', function () {
 
 gulp.task('watch', function () {
     gulp.watch('**/*.scss', ['sass']);
+    gulp.watch(['test/*.html'], browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'watch']);
+// run 'browser-sync' at startup; implicitly run 'sass'
+gulp.task('default', ['browser-sync', 'watch']);
