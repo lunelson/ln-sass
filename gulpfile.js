b6ff2ef8df54e992aa431f393ee0fa3eeda14741
Lu Nelson <lunelson@gmail.com>
2014-09-16 10:43:08 +0200

    gulpfile update for autoprefixer and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,27 @@
 // definitions //
 //////////////////
 
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
-//////////
-// sass //
-//////////
+//////////////////////////////////////
+// sass + autoprefixer + sourcemaps //
+//////////////////////////////////////
 
-// task: sass
 gulp.task('sass', function () {
     gulp.src('test/test.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             errLogToConsole: false,
-            onError: function(err) {
-                console.log(err);
-            }
+            onError: function(err) { console.log(err); }
         }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('test'));
 });
 
