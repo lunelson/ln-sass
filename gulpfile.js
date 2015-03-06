386e0f06affbfd917c722d3c670055d66bf8c3d4
Lu Nelson <lunelson@gmail.com>
2015-03-06 14:21:13 +0100

    codekit and gulp setup working

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,91 @@
-var buffer         = require('vinyl-buffer');
-var gulp           = require('gulp');
-var gutil          = require('gulp-util');
-var plumber        = require('gulp-plumber');
-var rename         = require("gulp-rename");
-var run            = require('gulp-run');
+var gulp   = require('gulp');
+var concat = require('gulp-concat');
 
-gulp.task('sass', function () {
-  gulp.src('test/test.scss', { buffer: false })
-    .pipe(plumber(function(err) {
-        gutil.beep();
-        var errorTxt = err.message +'\n\n'+ err.source;
-        gutil.log(gutil.colors.red(errorTxt));
-    }))
-    .pipe(run('/applications/libsass/sassc/bin/sassc -s', {verbosity: 1}))
-    .pipe(rename(function (path) { path.extname = ".css"; }))
-    .pipe(buffer())
-    .pipe(gulp.dest('test/'));
+gulp.task('mathsass',[], function() {
+    gulp.src([
+        'stylesheets/mathsass/dist/_constants.scss',
+        'stylesheets/mathsass/dist/functions/_pow.scss',
+        'stylesheets/mathsass/dist/functions/_fact.scss',
+        'stylesheets/mathsass/dist/functions/_sqrt.scss',
+        'stylesheets/mathsass/dist/functions/_exp.scss',
+        'stylesheets/mathsass/dist/functions/_frexp.scss',
+        'stylesheets/mathsass/dist/functions/_ldexp.scss',
+        'stylesheets/mathsass/dist/functions/_log.scss',
+        'stylesheets/mathsass/dist/functions/_sin.scss',
+        'stylesheets/mathsass/dist/functions/_cos.scss',
+        'stylesheets/mathsass/dist/functions/_tan.scss',
+        'stylesheets/mathsass/dist/functions/_csc.scss',
+        'stylesheets/mathsass/dist/functions/_sec.scss',
+        'stylesheets/mathsass/dist/functions/_cot.scss',
+        'stylesheets/mathsass/dist/functions/_asin.scss',
+        'stylesheets/mathsass/dist/functions/_acos.scss',
+        'stylesheets/mathsass/dist/functions/_atan.scss',
+        'stylesheets/mathsass/dist/functions/_atan2.scss',
+        'stylesheets/mathsass/dist/helpers/_strip-unit.scss',
+        'stylesheets/mathsass/dist/helpers/_deg-to-rad.scss',
+        'stylesheets/mathsass/dist/helpers/_rad-to-deg.scss',
+        'stylesheets/mathsass/dist/helpers/_unitless-rad.scss'
+        ])
+        .pipe(concat('_mathsass-pkgd.scss'))
+        .pipe(gulp.dest('stylesheets/mathsass/_pkg/'));
 });
+
+gulp.task('sass-util',[], function() {
+    gulp.src(['stylesheets/sass-util/stylesheets/*.scss'])
+        .pipe(concat('_sass-util-pkgd.scss'))
+        .pipe(gulp.dest('stylesheets/sass-util/_pkg/'));
+});
+
+gulp.task('sass-maps-plus',[], function() {
+    gulp.src([
+        'stylesheets/sass-maps-plus/stylesheets/_header.scss',
+        'stylesheets/sass-maps-plus/stylesheets/_utils.scss',
+        'stylesheets/sass-maps-plus/stylesheets/_maps.scss',
+        'stylesheets/sass-maps-plus/stylesheets/_multi-maps.scss',
+        'stylesheets/sass-maps-plus/stylesheets/_list-maps.scss',
+        'stylesheets/sass-maps-plus/stylesheets/_aliases.scss'
+        ])
+        .pipe(concat('_sass-maps-plus-pkgd.scss'))
+        .pipe(gulp.dest('stylesheets/sass-maps-plus/_pkg/'));
+});
+
+gulp.task('sass-ln',[], function() {
+    gulp.src([
+        'stylesheets/mathsass/_pkg/_mathsass-pkgd.scss',
+        'stylesheets/sass-util/_pkg/_sass-util-pkgd.scss',
+        'stylesheets/sass-maps-plus/_pkg/_sass-maps-plus-pkgd.scss',
+        'stylesheets/_reset.scss',
+        'stylesheets/_media.scss',
+        'stylesheets/_media-util.scss',
+        'stylesheets/_layout.scss',
+        'stylesheets/_layout-util.scss',
+        'stylesheets/_typo.scss'
+        ])
+        .pipe(concat('_sass-ln-pkgd.scss'))
+        .pipe(gulp.dest('_pkg/'));
+});
+
 
 ///////////
 // watch //
 ///////////
 
 gulp.task('watch', function () {
-    gulp.watch('**/*.scss', ['sass']);
+  // gulp.watch('sass-maps-plus/stylesheets/*.scss', ['sass-maps-plus']);
+  // gulp.watch('sass-ln/stylesheets/*.scss', ['sass-ln']);
+  gulp.watch(['stylesheets/**/*.scss', '!**/_pkg/**/*'], [
+      'mathsass',
+      'sass-util',
+      'sass-maps-plus',
+      'sass-ln',
+      'watch'
+  ]);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', [
+    'mathsass',
+    'sass-util',
+    'sass-maps-plus',
+    'sass-ln',
+    'watch'
+]);
