aa0938e6fa0b52f40e173465c161ddaed93d0332
Lu Nelson <lunelson@gmail.com>
2015-10-01 11:03:40 +0200

    update gulp, untrack css and map files

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,19 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', function () {
+    return gulp.src('src/**/*.css')
+});
 
 gulp.task('sass', function () {
   gulp.src(['./test/**/*.scss', '!./test/_holding/**/*.scss'], {base: './test'})
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./test'));
 });
 
