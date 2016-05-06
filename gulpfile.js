65cced4c53a7cbe737b86a957d4c283719fa115d
Lu Nelson <lunelson@gmail.com>
2016-05-06 15:48:37 +0200

    add gulp notify for testing

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@
 var gulp    = require('gulp');
 var mocha   = require('gulp-mocha');
 var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
 
 gulp.task('mocha', function() {
   gulp.src('test/test.js', {read: false})
-    .pipe(plumber())
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(mocha({reporter: 'spec'}));
 });
 
